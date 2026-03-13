59384192524e049dbc11bda201cbbbdfe7b14a12
Kirti Jain <kirtijain5285@gmail.com>
2026-03-13 11:08:07 +0530

    Fix port for deployment

--- app.py
+++ app.py
@@ -35,7 +35,8 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 # -----------------------------
 # Login Manager
 # -----------------------------
